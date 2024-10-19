bfafbf29a7dc12f4fbd0d6dbf808112b3d1bf8ed
Hrayr <138680803+HrayrHayrapetyan@users.noreply.github.com>
2024-10-19 19:52:47 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let fileInput=document.getElementById('file-input')
 let imageContainer=document.getElementById('images')
-let numOfFiles=document.getElementById('num-of-files')
+let numOfFiles=document.getElementById('num-of-files')  
 
 function preview(){
     imageContainer.innerHTML=''
-    numOfFiles.textContent=`${fileInput.files.length}Files Selected`;
+    numOfFiles.textContent=`${fileInput.files.length} Files Selected`;
     
-
-
-}
+    for (i of fileInput.files){
+        let reader=new FileReader()
+        let figure=document.createElement('figure')
+        let figCap=document.createElement('figcaption')
+        figCap.innerText=i.name
+        figure.appendChild(figCap)
+        reader.Onload=()=>{
+            let img=document.createElement('img')
+            img.setAtribute('src',reader.result)
+            figure.insertBefore(img,figCap)
+        }
+        imageContainer.appendChild(figure)
+        reader.readAsDaraURL(i)
+        
+    }
